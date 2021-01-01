f6665f6e3d46b9b054cffdbe0e7f4aa9cb19037b
FENR1 USER <fenr1user@biadsconsulting.org>
2021-01-01 22:47:22 +0600

    reactjs demo

--- reactjs_site/hello_world_reactjs/hello_world_app/src/App.js
+++ reactjs_site/hello_world_reactjs/hello_world_app/src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1> Hello React Js <Fun1 name="Nikhil Roy"/> </h1>
     </div>
   );
 }
 
+function Fun1(props){
+  return (
+    <>
+    I am {props.name}
+    </>
+  )
+}
 export default App;
